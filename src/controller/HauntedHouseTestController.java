85e82d97a880f1c8c8c082904c3e7770fdf12a3e
calvin <calvin@calvin-ThinkPad-T60>
2012-10-12 14:14:35 -0700

    Made HauntedHouseTestController compatible with JDK1.3 (the one FRC uses)

--- src/controller/HauntedHouseTestController.java
+++ src/controller/HauntedHouseTestController.java
@@ -4,46 +4,42 @@ import core.EventController;
 import core.HouseMech;
 import event.events.ButtonEvent;
 import event.listeners.ButtonListener;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Vector;
 import sensor.GRTXBoxJoystick;
 
 public class HauntedHouseTestController extends EventController implements ButtonListener {
 
     private GRTXBoxJoystick stick;
-    private List<HouseMech> mechanisms;
+    private Vector mechanisms;
     private int currentControlledMech = 0;	//The index of the current mechanism we are controlling.
 
-    public HauntedHouseTestController(List<HouseMech> mechanisms, GRTXBoxJoystick xbox, String name) {
+    public HauntedHouseTestController(Vector mechanisms, GRTXBoxJoystick xbox, String name) {
         super(name);
         stick = xbox;
         this.mechanisms = mechanisms;
     }
     
     public HauntedHouseTestController(GRTXBoxJoystick xbox, String name) {
-        this(new ArrayList<HouseMech>(), xbox, name);
+        this(new Vector(), xbox, name);
     }
     
     public void addMech(HouseMech mech){
         mechanisms.add(mech);
     }
 
-    @Override
     protected void startListening() {
         this.stick.addButtonListener(this);
     }
 
-    @Override
     protected void stopListening() {
         this.stick.removeButtonListener(this);
     }
 
-    @Override
     public void buttonPressed(ButtonEvent e) {
         
         switch (e.getButtonID()) {
             case GRTXBoxJoystick.KEY_BUTTON_0:
-                mechanisms.get(currentControlledMech).toggle();
+                ((HouseMech) mechanisms.get(currentControlledMech)).toggle();
                 break;
             case GRTXBoxJoystick.KEY_BUTTON_4:
                 currentControlledMech++;
@@ -55,7 +51,6 @@ public class HauntedHouseTestController extends EventController implements Butto
         currentControlledMech %= mechanisms.size();
     }
 
-    @Override
     public void buttonReleased(ButtonEvent e) {
         // TODO Auto-generated method stub
     }
