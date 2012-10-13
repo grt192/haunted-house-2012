ea2dde5440e23cf7fd500c9103463debe975e7f3
Trevor Nielsen <trevor.kn@gmail.com>
2012-10-13 11:06:23 -0700

    Renamed Mechanisms to mechanisms.

---
+++ src/mechanisms/SkeletonInChair.java
@@ -0,0 +1,47 @@
+package mechanisms;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import actuator.GRTSolenoid;
+import core.HouseMech;
+
+/**
+ *
+ * @author trevornielsen
+ */
+public class SkeletonInChair extends HouseMech {
+    
+    private GRTSolenoid chairPiston;
+    
+    /**
+     * 
+     * @param name
+     * @param chairPiston 
+     */
+    public SkeletonInChair(String name, GRTSolenoid chairPiston)
+    {
+        super(name, chairPiston);
+        
+        this.chairPiston = chairPiston;
+        
+    }
+    
+    @Override
+    public void extend()
+    {
+        chairPiston.engage(true);
+        super.extend();
+    }
+    
+    @Override
+    public void retract()
+    {
+        chairPiston.engage(false);
+        super.retract();
+    }
+    
+}
