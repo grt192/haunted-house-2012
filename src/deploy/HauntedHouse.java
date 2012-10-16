4b5cdd228ab20cefa4941d19d126b3a4d058cd94
Calvin Huang <clhuang@eneron.us>
2012-10-15 19:47:52 -0700

    Added entry point for haunted house, changed manifest to match

---
+++ src/deploy/HauntedHouse.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deploy;
+
+/**
+ * HauntedHouse is the entry point.
+ *
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the SimpleRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ * @author Calvin
+ */
+public class HauntedHouse {
+    
+    /**
+     * Constructor for the haunted house. 
+     * Initialize all components here.
+     */
+    public HauntedHouse() {
+        
+    }
+    
+}
