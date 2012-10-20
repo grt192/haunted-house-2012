3154e6214350d3fddb3ebac8bcbfb26d980e31c1
Calvin Huang <clhuang@eneron.us>
2012-10-19 18:20:51 -0700

    Autonomous behavior has been moved to HouseAutoController.

--- src/mechanisms/SkeletonInChair.java
+++ src/mechanisms/SkeletonInChair.java
@@ -18,36 +18,8 @@ public class SkeletonInChair extends HouseMech {
      * @param chairPiston
      */
     public SkeletonInChair(GRTSolenoid chairPiston) {
-        super("skeleton", chairPiston);
-        
-    }
-    
-    public void run()
-    {
-        while(running)
-        {
-            
-            try{
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                
-            }
-            
-            toggle();
-            
-        }
-    }
-    
-    public void startAutonomous()
-    {
-        startPolling();
-    }
-    
-    public void stopAutonomous()
-    {
-        halt();
-    }
-        
+        super("skeleton", chairPiston);    
+    }   
 
     protected void extend() {
         solenoids[0].engage(true);
