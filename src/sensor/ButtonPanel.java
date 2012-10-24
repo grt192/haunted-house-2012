ea83de235d1a1e40f8fe09d764a8e41023ddccc4
Calvin Huang <clhuang@eneron.us>
2012-10-23 19:45:10 -0700

    Added isPressed(button) functionality

--- src/sensor/ButtonPanel.java
+++ src/sensor/ButtonPanel.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package sensor;
 
 import core.Sensor;
@@ -117,6 +113,10 @@ public class ButtonPanel extends Sensor {
 
     public void removeButtonListener(ButtonListener b) {
         buttonListeners.removeElement(b);
+    }
+        
+    public boolean isPressed(int buttonID) {
+        return getState(buttonID) == PRESSED;
     }
 
     public synchronized void updateLEDs() {
